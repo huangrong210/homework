617011bcca0d6a626f117843505db53fcea8f6b1
hr226 <2108123704@qq.com>
2019-09-12 12:08:00 +0800

    changed app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-09-12 11:46:29
+ * @LastEditTime: 2019-09-12 11:46:29
+ * @LastEditors: your name
+ */
+console.log("Hello World!");
