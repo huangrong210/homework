6cf9eddc46acb7266aa0f5448642473ee2de86b4
hr226 <2108123704@qq.com>
2019-09-12 12:25:09 +0800

    another changed

---
+++ dir2/app2.js
@@ -0,0 +1,8 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-09-12 12:21:24
+ * @LastEditTime: 2019-09-12 12:21:24
+ * @LastEditors: your name
+ */
+console.log('dir2');
